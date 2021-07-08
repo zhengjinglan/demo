7c4143e403e7b0bc8b217f9357416d666be986ad
zhengjinglan <zjl_lan@126.com>
2021-07-08 17:20:06 +0800

    no message

--- unit_demo/unit_for_test.py
+++ unit_demo/unit_for_test.py
@@ -3,7 +3,21 @@
 import unittest
 from selenium import webdriver
 from time import sleep
+from ddt import ddt, data, unpack, file_data
 
+def read_file():
+    file = open('params.txt', 'r', encoding= 'utf8')
+    # file = open('test1.yaml', 'r', encoding='utf8')
+    li = []
+    for line in file.readlines():
+        # print(line)
+        li.append(line.strip('\n').split(','))
+    # for l in li:
+        # print(l)
+    file.close()
+    return li
+
+@ddt
 class UnitForTest(unittest.TestCase):
     #前置条件（初始化）
     def setUp(self) -> None:
@@ -16,22 +30,28 @@ class UnitForTest(unittest.TestCase):
         self.driver.quit()
 
     #定义测试用例校验账号密码2
-    def test_2(self):
-        # driver = webdriver.Chrome()
-        # driver.get('http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html')
-        # driver.implicitly_wait(10)
-        self.driver.find_element_by_name('accounts').send_keys('123456')
-        self.driver.find_element_by_name('pwd').send_keys('123456')
+    # @data(['666666', '123456'], ['123456', '123456'], ['123456', '666666']) #传多个参数要用中括号
+    @data(*read_file())
+    @unpack #解包 @装饰器
+    def test_2(self, username, pwd):
+        self.driver.find_element_by_name('accounts').send_keys(username)
+        self.driver.find_element_by_name('pwd').send_keys(pwd)
         self.driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[2]/form/div[3]/button').click()
-        sleep(5)
-    #校验账号密码1
-    def test_1(self):
-        # driver = webdriver.Chrome()
-        # driver.get('http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html')
-        # driver.implicitly_wait(10)
-        self.driver.find_element_by_name('accounts').send_keys('666666')
-        self.driver.find_element_by_name('pwd').send_keys('666666')
-        self.driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[2]/form/div[3]/button').click()
-        sleep(5)
+        sleep(2)
+
+    #一般函数，用于让测试用例调用的函数，只有在调用的时候才会生效
+    def plus(self):
+        s = 1
+        b = 2
+        return s + b
+
+    #传入的参数是字典格式
+    @file_data('test1.yaml')
+    def test_1(self, **user):
+        name = user.get('name')
+        pwd = user.get('pwd')
+        print(name)
+        print(pwd)
+
 if __name__ == '__main__':
     unittest.main()
