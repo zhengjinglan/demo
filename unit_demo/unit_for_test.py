8e30dbc21c2089026ff55c09168071bc1795d299
zhengjinglan <zjl_lan@126.com>
2021-07-08 15:24:59 +0800

    no message

--- unit_demo/unit_for_test.py
+++ unit_demo/unit_for_test.py
@@ -1,30 +1,37 @@
 # _*_encoding:'utf-8' _*_
 #导入unittest包
 import unittest
+from selenium import webdriver
+from time import sleep
 
 class UnitForTest(unittest.TestCase):
-    #class前置条件
-    @classmethod
-    def setUpClass(cls) -> None:
-        print('setUpClass')
-    #class后置条件
-    @classmethod
-    def tearDownClass(cls) -> None:
-        print('tesrDownClass')
-    #前置条件
+    #前置条件（初始化）
     def setUp(self) -> None:
-        print('setUp')
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html')
+        self.driver.implicitly_wait(10)
 
-    #后置条件
+    #后置条件(执行完成关闭浏览器页面）
     def tearDown(self) -> None:
-        print('tearDown')
+        self.driver.quit()
 
-    #定义测试用例
+    #定义测试用例校验账号密码2
     def test_2(self):
-        print("this is test2")
-
+        # driver = webdriver.Chrome()
+        # driver.get('http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html')
+        # driver.implicitly_wait(10)
+        self.driver.find_element_by_name('accounts').send_keys('123456')
+        self.driver.find_element_by_name('pwd').send_keys('123456')
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[2]/form/div[3]/button').click()
+        sleep(5)
+    #校验账号密码1
     def test_1(self):
-        print("this is test1")
-
+        # driver = webdriver.Chrome()
+        # driver.get('http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html')
+        # driver.implicitly_wait(10)
+        self.driver.find_element_by_name('accounts').send_keys('666666')
+        self.driver.find_element_by_name('pwd').send_keys('666666')
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div[2]/form/div[3]/button').click()
+        sleep(5)
 if __name__ == '__main__':
     unittest.main()
